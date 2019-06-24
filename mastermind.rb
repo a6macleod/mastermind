563a6f833fdc7bc5b1891404baf6c7382d81b29b
Andrew MacLeod <A6macleod@gmail.com>
2019-06-24 17:44:15 -0400

    Updated end game sequences

--- mastermind.rb
+++ mastermind.rb
@@ -7,19 +7,34 @@
 # 6 colors; Green, Blue, Red, Yellow, Orange, Purple
 
 class Mastermind
-	attr_accessor :hidden_code, :win_game, :turn_counter, :human_picks_secret_code
+	attr_accessor :hidden_code, :game_is_over, :turn_counter, :human_picks_secret_code, :player_color_array
 
 	def initialize
 		@hidden_code = []
-		@win_game = false
+		@game_is_over = false
 		@turn_counter = 0
 		@human_picks_secret_code = false
+		@player_color_array = []
 	end
 
-	def who_picks(computer_or_human)
-		if computer_or_human[0] == "y"
-			self.human_picks_secret_code = true		
+	def player_number_pick
+		display_color_choices
+		number_array = []
+		counter = 0
+		4.times do
+			counter += 1
+			print "Spot #{counter}: "
+			input = gets.chomp.to_i
+			if (1..6).include?(input) && number_array.include?(input) == false
+				number_array << input
+			else
+				puts "please enter one number and 'enter'"
+				redo
+			end
 		end
+		player_color_array = numbers_to_colors(number_array)
+		p "You picked #{player_color_array.join(", ")}"
+		return player_color_array
 	end
 
 	def hidden_code_pick
@@ -38,12 +53,12 @@ class Mastermind
 	def compare_code(code_guess, hidden_code)
 		matches = code_guess & hidden_code
 		matched_code = []
-		win_check = 0
+		match_all_four = 0
 		i = 0
 		4.times do 
 			if code_guess[i] == hidden_code[i]
 				matched_code << code_guess[i]
-				win_check += 1
+				match_all_four += 1
 			else
 				matched_code << "x"
 			end
@@ -54,12 +69,12 @@ class Mastermind
 		puts "CODE MATCH: #{matched_code}"
 		puts "Guess number #{turn_counter + 1} of 12"
 		puts "\n--------------------------------------------------------------\n\n"
-		win?(win_check)
+		game_over?(match_all_four)
 	end
 
-	def win?(win_check)
-		if win_check == 4
-			self.win_game = true
+	def game_over?(match_all_four)
+		if match_all_four == 4
+			self.game_is_over = true
 		end
 	end
 
@@ -96,64 +111,48 @@ class Mastermind
 	end
 end
 
-class HumanCodePick < Mastermind
-	attr_accessor :player_color_array
-
-	def initialize
-		@player_color_array
-	end
-
-	def player_number_pick
-		display_color_choices
-		number_array = []
-		4.times do
-			input = gets.chomp.to_i
-			if (1..6).include?(input) && number_array.include?(input) == false
-				number_array << input
-			else
-				puts "please enter one number and 'enter'"
-				redo
-			end
-		end
-		player_color_array = numbers_to_colors(number_array)
-		p "You picked #{player_color_array.join(", ")}"
-		return player_color_array
-	end
-
-end
-
 def start_game
-	human_code = HumanCodePick.new
 	current_game = Mastermind.new
-	puts "Do you want to pick the secret code? (y/n)"
-	computer_or_human = gets.chomp.downcase
-	current_game.who_picks(computer_or_human)
-	play_game(current_game, human_code)
+	who_picks(current_game)
+	play_game(current_game)
 end
 
-def guess_picks(current_game, human_code)
-	if current_game.human_picks_secret_code == true
-		return human_code.player_number_pick # player picks secret code
+def guess_picks(current_game)
+	if current_game.human_picks_secret_code == false
+		return current_game.player_number_pick # player picks secret code
 	 else
 	 	return current_game.computer_code_pick # computer picks secret code
 	 end 
 end
 
-def play_game(current_game, human_code)
+def who_picks(current_game)
+	puts "Do you want to pick the secret code? (y/n)"
+	computer_or_human = gets.chomp.downcase
+	if computer_or_human[0] == "y"
+		current_game.human_picks_secret_code = true
+	elsif computer_or_human == "n"
+		return
+	else
+		puts "Please enter a 'y' or 'n'"
+	end
+	current_game.hidden_code_pick
+end
+
+def play_game(current_game)
 	12.times do
-		code_guess = guess_picks(current_game, human_code)
+		code_guess = guess_picks(current_game)
 		puts "hidden code #{current_game.hidden_code}"
 		current_game.compare_code(code_guess, current_game.hidden_code)
 		puts "press enter to continue"
 		pause_game = gets.chomp
-		if current_game.win_game == true 
-			winner_message
+		if current_game.game_is_over == true 
+			code_has_been_matched(current_game)
 			break
 		else
 			 current_game.turn_counter += 1
 		end
 		if current_game.turn_counter == 12 
-			loser_message
+			turn_counter_maxed(current_game)
 			break
 		else next
 		end
@@ -162,12 +161,20 @@ def play_game(current_game, human_code)
 	puts "\n\t~GAME OVER~\n\n\n\n\n\n\n\n\n"
 end
 
-def winner_message
-	puts "Congratulations! you matched the hidden code!\n"
+def code_has_been_matched(current_game)
+	if current_game.human_picks_secret_code = false
+		puts "You lose! The computer guessed your code"
+	else
+		puts "Congratulations! you won!\n"
+	end
 end
 
-def loser_message
-	puts "Sorry you lost. Better luck next time!"
+def turn_counter_maxed(current_game)
+	if current_game.human_picks_secret_code = true
+		puts "congratulations! The computer didn't guess your code!"
+	else
+		puts "Sorry you lost. Better luck next time!"
+	end
 end
 
 puts "\n\n\n--------------------------------------------------------------"
