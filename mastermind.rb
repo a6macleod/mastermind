6fb2af963b03a756d01000d5e6461af248005e71
Andrew MacLeod <A6macleod@gmail.com>
2019-06-26 17:31:18 -0400

    Add computer guessing logic

--- mastermind.rb
+++ mastermind.rb
@@ -7,14 +7,16 @@
 # 6 colors; Green, Blue, Red, Yellow, Orange, Purple
 
 class Mastermind
-	attr_accessor :hidden_code, :game_is_over, :turn_counter, :human_picks_secret_code, :player_color_array
+	attr_accessor :hidden_code, :game_is_over, :turn_counter, :human_picks_secret_code, :computer_matched_colors, :computer_matched_secret_code, :possible_colors
 
 	def initialize
 		@hidden_code = []
 		@game_is_over = false
 		@turn_counter = 0
 		@human_picks_secret_code = false
-		@player_color_array = []
+		@matched_colors = []
+		@matched_secret_code = ["x", "x", "x", "x"]
+		@possible_colors = ["green", "blue", "red", "yellow", "orange", "purple"]
 	end
 
 	def player_number_pick
@@ -38,35 +40,60 @@ class Mastermind
 	end
 
 	def hidden_code_pick
+		puts human_picks_secret_code
 		if human_picks_secret_code == false
-			self.hidden_code = computer_code_pick
+			self.hidden_code = computer_pick_secret_code
 		else
 			self.hidden_code = player_number_pick
 		end
 	end
 
-	def computer_code_pick
+	def computer_code_guess
+		computer_guess = []
+		unmatched_color_pool = (possible_colors - matched_colors)
+		matched_color_pool = (matched_colors - matched_secret_code)
+		j = 0
+		until j == 4			
+			if matched_secret_code[j] == "x" 
+				if matched_color_pool.size > 0
+					color_to_add = matched_color_pool.sample
+					matched_color_pool = (matched_color_pool - color_to_add)
+					computer_guess << color_to_add
+				else 
+					color_to_add = unmatched_color_pool.sample
+					unmatched_color_pool = (unmatched_color_pool - color_to_add) 
+					computer_guess << color_to_add
+				end
+			else
+				computer_guess << matched_secret_code[j]
+			end
+			postion += 1
+		end
+		return computer_guess
+	end
+
+	def computer_pick_secret_code
+		computer_matched_secret_code << matched_color
 		computer_number_array = (1..6).to_a.shuffle.take(4)
 		numbers_to_colors(computer_number_array)
 	end
 
 	def compare_code(code_guess, hidden_code)
-		matches = code_guess & hidden_code
-		matched_code = []
+		self.matched_colors = code_guess & hidden_code
 		match_all_four = 0
 		i = 0
 		4.times do 
 			if code_guess[i] == hidden_code[i]
-				matched_code << code_guess[i]
+				self.matched_secret_code[i] = code_guess[i]
 				match_all_four += 1
 			else
-				matched_code << "x"
+				self.matched_secret_code[i] = "x"
 			end
 			i += 1
 		end
 		puts "\n\n--------------------------------------------------------------\n\n"
-		puts "MATCHED COLORS: #{matches}"
-		puts "CODE MATCH: #{matched_code}"
+		puts "MATCHED COLORS: #{matched_colors}"
+		puts "CODE MATCH: #{matched_secret_code}"
 		puts "Guess number #{turn_counter + 1} of 12"
 		puts "\n--------------------------------------------------------------\n\n"
 		game_over?(match_all_four)
@@ -118,25 +145,33 @@ def start_game
 end
 
 def guess_picks(current_game)
-	if current_game.human_picks_secret_code == false
-		return current_game.player_number_pick # player picks secret code
+	if current_game.human_picks_secret_code == true
+		return current_game.player_number_pick 
 	 else
-	 	return current_game.computer_code_pick # computer picks secret code
+	 	return current_game.computer_code_guess 
 	 end 
 end
 
 def who_picks(current_game)
-	puts "Do you want to pick the secret code? (y/n)"
+	puts "\nDo you want to pick the secret code? (y/n)\n"
 	computer_or_human = gets.chomp.downcase
 	if computer_or_human[0] == "y"
 		current_game.human_picks_secret_code = true
 	elsif computer_or_human == "n"
 		return
 	else
-		puts "Please enter a 'y' or 'n'"
+		who_picks_error(current_game)
 	end
 	current_game.hidden_code_pick
 end
+
+def who_picks_error(current_game)
+	puts"\n\n\n--------------------------------------------------------------"
+	puts "--------------------------------------------------------------\n\n"
+	puts "Please enter a 'y' or 'n'\n\n"
+	who_picks(current_game)
+end
+
 
 def play_game(current_game)
 	12.times do
